e8921283ffee46798aa2ce4515714a7ae469167d
Weverton procópio <wevertonprocopio690@gmail.com>
2021-01-26 18:03:26 -0300

    test each route for potential match

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+async function route() {
+  const routes = [
+    { path: "/", view: () => console.log("Hello i'm dashboard") },
+    { path: "/posts", view: () => console.log("Hello i'm Posts") },
+    { path: "/settings", view: () => console.log("Hello i'm settings") },
+  ];
+
+  //   teste de  cada rota para correspondência
+  const potentialMatch = routes.map((route) => {
+    return {
+      route,
+      isMatch: location.pathname === route.path,
+    };
+  });
+}
