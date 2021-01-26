55740e0eb58eed4b25daf9f1766e6e3e321098c4
Weverton procópio <wevertonprocopio690@gmail.com>
2021-01-26 19:30:15 -0300

    project end

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,42 +1,73 @@
+import Dashboard from "./views/Dashboard.js";
+import Posts from "./views/Posts.js";
+import PostView from "./views/PostView.js";
+import Settings from "./views/Settings.js";
+
+const PathToRegex = (path) =>
+  new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+function GetParams(match) {
+  const values = match.result.slice(1);
+  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(
+    (result) => result[1]
+  );
+
+  return Object.fromEntries(
+    keys.map((key, i) => {
+      return [key, values[i]];
+    })
+  );
+}
+
 function NavigateTo(url) {
   history.pushState(null, null, url);
   Router();
 }
+
 async function Router() {
   const routes = [
-    { path: "/", view: () => console.log("Hello i'm dashboard") },
-    { path: "/posts", view: () => console.log("Hello i'm Posts") },
-    { path: "/settings", view: () => console.log("Hello i'm settings") },
+    { path: "/", view: Dashboard },
+    { path: "/posts", view: Posts },
+    { path: "/posts/:id", view: PostView },
+    { path: "/settings", view: Settings },
   ];
 
   //   teste de  cada rota para correspondência
   const potentialMatches = routes.map((route) => {
     return {
-      route,
-      isMatch: location.pathname === route.path,
+      route: route,
+      result: location.pathname.match(PathToRegex(route.path)),
     };
   });
 
-  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch);
+  let match = potentialMatches.find(
+    (potentialMatch) => potentialMatch.result !== null
+  );
 
   if (!match) {
     match = {
       route: routes[0],
-      isMatch: true,
+      result: [location.pathname],
     };
   }
 
-  console.log(match.route.view());
+  const view = new match.route.view(GetParams(match));
+
+  document.querySelector("#app").innerHTML = await view.getHtml();
+}
+
+function PreventRefreshToClickOnLink(e) {
+  if (e.target.matches("[data-link]")) {
+    e.preventDefault();
+    NavigateTo(e.target.href);
+  }
 }
 
 window.addEventListener("popstate", Router);
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.addEventListener("click", (e) => {
-    if (e.target.matches("[data-link]")) {
-      e.preventDefault();
-      NavigateTo(e.target.href);
-    }
-  });
+  document.body.addEventListener("click", (e) =>
+    PreventRefreshToClickOnLink(e)
+  );
   Router();
 });
